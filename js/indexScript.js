f411b02f32570d4321e545afbd4298f2a06d4ca9
SakeTora <jakeasa123@gmail.com>
2022-08-13 18:10:34 +0800

    Update funtion
    Update that could save and load data by string

--- js/indexScript.js
+++ js/indexScript.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
     document.getElementById('testImage').addEventListener('click', function (e) {
         whiteEyeCount += 1
-        if (whiteEyeCount == 5) {
+        if (whiteEyeCount >= 5) {
             alert("ㄋ豪瑟ㄛ")
             window.location.href = "https://saketora95.github.io/ElsDataSheet/combi_calc.html"
         }
---
+++ js/saveLoadScript.js
@@ -0,0 +1,141 @@
+var int_formatter = new Intl.NumberFormat('en-US')
+
+function saveData() {
+    var tempString = ""
+
+    // Job
+    tempString += document.getElementById("inputJobAtkPower").value
+    tempString += ", " + document.getElementById("inputJobCritDmg").value
+    tempString += ", " + document.getElementById("inputJobBossDmg").value
+    tempString += ", " + document.getElementById("inputJobSkillDmg").value
+    tempString += ", " + document.getElementById("inputJobAllSkillDmg").value
+
+    // Weapon
+    tempString += ", " + document.getElementById("inputWeaponType").value
+    tempString += ", " + document.getElementById("inputWeaponUpgrade").value
+    tempString += ", " + document.getElementById("inputWeaponStage").value
+    tempString += ", " + document.getElementById("inputWeaponAtkPower").value
+    tempString += ", " + document.getElementById("inputWeaponCritDmg").value
+
+    // Equip
+    tempString += ", " + document.getElementById("inputEquipRadioAmet").checked
+    tempString += ", " + document.getElementById("inputEquipRadioTene").checked
+    tempString += ", " + document.getElementById("inputEquipRadioBoth").checked
+    tempString += ", " + document.getElementById("checkBoxAmetType").checked
+    tempString += ", " + document.getElementById("inputEquipUpgradeTopAmet").value
+    tempString += ", " + document.getElementById("inputEquipReforgeTopAmet").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeBottomAmet").value
+    tempString += ", " + document.getElementById("inputEquipReforgeBottomAmet").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeGloveAmet").value
+    tempString += ", " + document.getElementById("inputEquipReforgeGloveAmet").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeShoesAmet").value
+    tempString += ", " + document.getElementById("inputEquipReforgeShoesAmet").value
+    tempString += ", " + document.getElementById("checkBoxTeneType").checked
+    tempString += ", " + document.getElementById("inputEquipUpgradeTopTene").value
+    tempString += ", " + document.getElementById("inputEquipReforgeTopTene").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeBottomTene").value
+    tempString += ", " + document.getElementById("inputEquipReforgeBottomTene").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeGloveTene").value
+    tempString += ", " + document.getElementById("inputEquipReforgeGloveTene").value
+    tempString += ", " + document.getElementById("inputEquipUpgradeShoesTene").value
+    tempString += ", " + document.getElementById("inputEquipReforgeShoesTene").value
+
+    // Other
+    tempString += ", " + document.getElementById("inputOtherBossDmgStone").value
+    tempString += ", " + document.getElementById("inputOtherDeBuff").value
+    tempString += ", " + document.getElementById("inputOtherReson").value
+
+    // Personal
+    tempString += ", " + document.getElementById("inputPerAtkPower").value
+    tempString += ", " + document.getElementById("inputPerCritDmg").value
+    tempString += ", " + document.getElementById("inputPerBossDmg").value
+    tempString += ", " + document.getElementById("inputPerSkillDmg").value
+    tempString += ", " + document.getElementById("inputPerAllSkillDmg").value
+    tempString += ", " + document.getElementById("inputPerPolar").value
+    tempString += ", " + document.getElementById("inputPerAdapt").value
+    
+    // CheckBox
+    tempString += ", " + document.getElementById("checkBoxCooldown").checked
+    tempString += ", " + document.getElementById("checkBoxVer55").checked
+
+    // Result
+    var inputSaveData = document.getElementById("inputSaveData")
+    inputSaveData.value = tempString
+}
+
+function copyData() {
+    navigator.clipboard.writeText(document.getElementById("inputSaveData").value)
+}
+
+function loadData() {
+    var inputData = document.getElementById("inputLoadData").value
+    if (inputData != "") {
+        inputData = inputData.split(", ")
+        
+        // Job
+        document.getElementById("inputJobAtkPower").value = inputData[0]
+        document.getElementById("inputJobCritDmg").value = inputData[1]
+        document.getElementById("inputJobBossDmg").value = inputData[2]
+        document.getElementById("inputJobSkillDmg").value = inputData[3]
+        document.getElementById("inputJobAllSkillDmg").value = inputData[4]
+
+        // Weapon
+        document.getElementById("inputWeaponType").value = inputData[5]
+        document.getElementById("inputWeaponUpgrade").value = inputData[6]
+        document.getElementById("inputWeaponStage").value = inputData[7]
+        document.getElementById("inputWeaponAtkPower").value = inputData[8]
+        document.getElementById("inputWeaponCritDmg").value = inputData[9]
+
+        // Equip
+        document.getElementById("inputEquipRadioAmet").checked = parseBoolean(inputData[10])
+        document.getElementById("inputEquipRadioTene").checked = parseBoolean(inputData[11])
+        document.getElementById("inputEquipRadioBoth").checked = parseBoolean(inputData[12])
+        document.getElementById("checkBoxAmetType").checked = parseBoolean(inputData[13])
+        document.getElementById("inputEquipUpgradeTopAmet").value = inputData[14]
+        document.getElementById("inputEquipReforgeTopAmet").value = inputData[15]
+        document.getElementById("inputEquipUpgradeBottomAmet").value = inputData[16]
+        document.getElementById("inputEquipReforgeBottomAmet").value = inputData[17]
+        document.getElementById("inputEquipUpgradeGloveAmet").value = inputData[18]
+        document.getElementById("inputEquipReforgeGloveAmet").value = inputData[19]
+        document.getElementById("inputEquipUpgradeShoesAmet").value = inputData[20]
+        document.getElementById("inputEquipReforgeShoesAmet").value = inputData[21]
+        document.getElementById("checkBoxTeneType").checked = parseBoolean(inputData[22])
+        document.getElementById("inputEquipUpgradeTopTene").value = inputData[23]
+        document.getElementById("inputEquipReforgeTopTene").value = inputData[24]
+        document.getElementById("inputEquipUpgradeBottomTene").value = inputData[25]
+        document.getElementById("inputEquipReforgeBottomTene").value = inputData[26]
+        document.getElementById("inputEquipUpgradeGloveTene").value = inputData[27]
+        document.getElementById("inputEquipReforgeGloveTene").value = inputData[28]
+        document.getElementById("inputEquipUpgradeShoesTene").value = inputData[29]
+        document.getElementById("inputEquipReforgeShoesTene").value = inputData[30]
+
+        // Other
+        document.getElementById("inputOtherBossDmgStone").value = inputData[31]
+        document.getElementById("inputOtherDeBuff").value = inputData[32]
+        document.getElementById("inputOtherReson").value = inputData[33]
+
+        // Personal
+        document.getElementById("inputPerAtkPower").value = inputData[34]
+        document.getElementById("inputPerCritDmg").value = inputData[35]
+        document.getElementById("inputPerBossDmg").value = inputData[36]
+        document.getElementById("inputPerSkillDmg").value = inputData[37]
+        document.getElementById("inputPerAllSkillDmg").value = inputData[38]
+        document.getElementById("inputPerPolar").value = inputData[39]
+        document.getElementById("inputPerAdapt").value = inputData[40]
+        
+        // CheckBox
+        document.getElementById("checkBoxCooldown").checked = parseBoolean(inputData[41])
+        document.getElementById("checkBoxVer55").checked = parseBoolean(inputData[42])
+    }
+}
+
+function parseBoolean(input_var) {
+    if (input_var == "true") {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
