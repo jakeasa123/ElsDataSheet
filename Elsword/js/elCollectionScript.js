68ed2d1484a685c9da17c9ed7d8d876672417ec6
saketora95 <jakeasa123@gmail.com>
2023-01-12 09:44:33 +0800

    修正數值為 0 時仍會創建欄位的問題

--- Elsword/js/elCollectionScript.js
+++ Elsword/js/elCollectionScript.js
@@ -37,61 +37,62 @@ function initialTable() {
                         var magicStone = cookieDict[tempId][0]
                         var secretReward = parseInt(cookieDict[tempId][1])
 
-                        // tr
-                        var trId = "tr_" + tempId
+                        if (magicStone > 0 || secretReward > 0) {
+                            // tr
+                            var trId = "tr_" + tempId
 
-                        var tempChild = document.createElement('tr')
-                        tempChild.setAttribute("id", trId)
-                        document.getElementById("jobTableBody").appendChild(tempChild)
+                            var tempChild = document.createElement('tr')
+                            tempChild.setAttribute("id", trId)
+                            document.getElementById("jobTableBody").appendChild(tempChild)
 
-                        // th
-                        tempChild = document.createElement('th')
-                        tempChild.setAttribute("id", "th_" + tempId)
-                        tempChild.setAttribute("class", "text-center")
-                        tempChild.setAttribute("scope", "row")
-                        tempChild.textContent = charName
-                        document.getElementById(trId).appendChild(tempChild)
+                            // th
+                            tempChild = document.createElement('th')
+                            tempChild.setAttribute("id", "th_" + tempId)
+                            tempChild.setAttribute("class", "text-center")
+                            tempChild.setAttribute("scope", "row")
+                            tempChild.textContent = charName
+                            document.getElementById(trId).appendChild(tempChild)
 
-                        // td - Line
-                        tempChild = document.createElement('td')
-                        tempChild.setAttribute("id", "td_" + tempId)
-                        tempChild.setAttribute("class", "text-center")
-                        tempChild.textContent = pathName
-                        document.getElementById(trId).appendChild(tempChild)
+                            // td - Line
+                            tempChild = document.createElement('td')
+                            tempChild.setAttribute("id", "td_" + tempId)
+                            tempChild.setAttribute("class", "text-center")
+                            tempChild.textContent = pathName
+                            document.getElementById(trId).appendChild(tempChild)
 
-                        // td - Magic Stone
-                        tempChild = document.createElement('td')
-                        tempChild.setAttribute("id", "td_" + tempId + "_MagicStone")
-                        tempChild.setAttribute("class", "text-center")
-                        document.getElementById(trId).appendChild(tempChild)
+                            // td - Magic Stone
+                            tempChild = document.createElement('td')
+                            tempChild.setAttribute("id", "td_" + tempId + "_MagicStone")
+                            tempChild.setAttribute("class", "text-center")
+                            document.getElementById(trId).appendChild(tempChild)
 
-                        tempChild = document.createElement('input')
-                        tempChild.setAttribute("id", "input_" + tempId + "_MagicStone")
-                        tempChild.setAttribute("class", "form-control")
-                        tempChild.setAttribute("value", magicStone)
-                        tempChild.setAttribute("onchange", "inputOnChange(\'" + tempId + "\')")
-                        document.getElementById("td_" + tempId + "_MagicStone").appendChild(tempChild)
+                            tempChild = document.createElement('input')
+                            tempChild.setAttribute("id", "input_" + tempId + "_MagicStone")
+                            tempChild.setAttribute("class", "form-control")
+                            tempChild.setAttribute("value", magicStone)
+                            tempChild.setAttribute("onchange", "inputOnChange(\'" + tempId + "\')")
+                            document.getElementById("td_" + tempId + "_MagicStone").appendChild(tempChild)
 
-                        // td - Secret Reward
-                        tempChild = document.createElement('td')
-                        tempChild.setAttribute("id", "td_" + tempId + "_SecretReward")
-                        tempChild.setAttribute("class", "text-center")
-                        document.getElementById(trId).appendChild(tempChild)
+                            // td - Secret Reward
+                            tempChild = document.createElement('td')
+                            tempChild.setAttribute("id", "td_" + tempId + "_SecretReward")
+                            tempChild.setAttribute("class", "text-center")
+                            document.getElementById(trId).appendChild(tempChild)
 
-                        tempChild = document.createElement('button')
-                        tempChild.setAttribute("id", "button_" + tempId + "_SecretReward")
-                        tempChild.setAttribute("type", "form-button")
-                        if (secretReward > 1) {
-                            tempChild.setAttribute("class", "btn btn-outline-primary")
-                        } else if (secretReward == 1) {
-                            tempChild.setAttribute("class", "btn btn-warning")
-                        } else {
-                            tempChild.setAttribute("class", "btn btn-danger")
+                            tempChild = document.createElement('button')
+                            tempChild.setAttribute("id", "button_" + tempId + "_SecretReward")
+                            tempChild.setAttribute("type", "form-button")
+                            if (secretReward > 1) {
+                                tempChild.setAttribute("class", "btn btn-outline-primary")
+                            } else if (secretReward == 1) {
+                                tempChild.setAttribute("class", "btn btn-warning")
+                            } else {
+                                tempChild.setAttribute("class", "btn btn-danger")
+                            }
+                            tempChild.textContent = "剩餘 " + secretReward + " 次"
+                            tempChild.setAttribute("onclick", "inputOnClick(\'" + tempId + "\')")
+                            document.getElementById("td_" + tempId + "_SecretReward").appendChild(tempChild)
                         }
-                        tempChild.textContent = "剩餘 " + secretReward + " 次"
-                        tempChild.setAttribute("onclick", "inputOnClick(\'" + tempId + "\')")
-                        document.getElementById("td_" + tempId + "_SecretReward").appendChild(tempChild)
-
                     }
                 });
             });
