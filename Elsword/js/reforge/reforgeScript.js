22391d6fcadb83e4c1bc12fff373a10d930e8a71
Tora95 <jakeasa123@gmail.com>
2023-07-06 11:13:22 +0800

    Update reforge data table

--- Elsword/js/reforge/reforgeScript.js
+++ Elsword/js/reforge/reforgeScript.js
@@ -1,18 +1,18 @@
 
 function numberToStringWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function roundNumber(num, decimal) {
-    return Math.round( ( num + Number.EPSILON ) * Math.pow( 10, decimal ) ) / Math.pow( 10, decimal )
+    return Math.round( ( num + Number.EPSILON ) * Math.pow( 10, decimal ) ) / Math.pow( 10, decimal );
 }
 
 function executeCalc() {
     // Get input
-    const equipRadio = document.querySelector('input[name="radioEquipType"]:checked').value
+    const equipRadio = document.querySelector('input[name="radioEquipType"]:checked').value;
     const equipType = (equipRadio === 'radioAmet' ? 'Amethystine' : 'Tenebrous');
-    var currentReforge = document.getElementById("currentReforge").value
-    var targetReforge = document.getElementById("targetReforge").value
+    var currentReforge = document.getElementById('currentReforge').value;
+    var targetReforge = document.getElementById('targetReforge').value;
 
     // Stop when not number input.
     if (isNaN(currentReforge) || isNaN(targetReforge)) {
@@ -23,31 +23,31 @@ function executeCalc() {
     }
 
     // Parse input and update display string
-    currentReforge = parseInt(currentReforge)
-    targetReforge = parseInt(targetReforge)
+    currentReforge = parseInt(currentReforge);
+    targetReforge = parseInt(targetReforge);
     if (equipType === 'Amethystine') {
         // Amethystine
-        document.getElementById("tEquipType").innerHTML = "虹霓防具 " + currentReforge + " → " + targetReforge;
-        document.getElementById("tReforgeMaterial_1").innerHTML = "虹霓珠";
-        document.getElementById("tReforgeMaterial_2").innerHTML = "冰塊";
-        document.getElementById("tRequireDaysLbl_1").innerHTML = "所需天數 (350 / 週)";
-        document.getElementById("tRequireDaysFullLbl_1").innerHTML = "所需天數 (550 / 週)";
-        document.getElementById("tRequireDaysLbl_2").innerHTML = "所需天數 (350 / 週)";
-        document.getElementById("tRequireDaysFullLbl_2").innerHTML = "所需天數 (550 / 週)";
+        document.getElementById('tEquipType').innerHTML = '虹霓防具 ' + currentReforge + ' → ' + targetReforge;
+        document.getElementById('tReforgeMaterial_1').innerHTML = '虹霓珠';
+        document.getElementById('tReforgeMaterial_2').innerHTML = '冰塊';
+        document.getElementById('tRequireDaysLbl_1').innerHTML = '所需天數 (350 / 週)';
+        document.getElementById('tRequireDaysFullLbl_1').innerHTML = '所需天數 (550 / 週)';
+        document.getElementById('tRequireDaysLbl_2').innerHTML = '所需天數 (350 / 週)';
+        document.getElementById('tRequireDaysFullLbl_2').innerHTML = '所需天數 (550 / 週)';
     } else {
         // Tenebrous
-        document.getElementById("tEquipType").innerHTML = "泰納防具 " + currentReforge + " → " + targetReforge
-        document.getElementById("tReforgeMaterial_1").innerHTML = "泰納布洛斯氣息"
-        document.getElementById("tReforgeMaterial_2").innerHTML = "塔斯瑪氣息"
-        document.getElementById("tRequireDaysLbl_1").innerHTML = "-"
-        document.getElementById("tRequireDaysFullLbl_1").innerHTML = "所需天數 (2,100 / 週)"
-        document.getElementById("tRequireDaysLbl_2").innerHTML = "-"
-        document.getElementById("tRequireDaysFullLbl_2").innerHTML = "所需天數 (2,100 / 週)"
+        document.getElementById('tEquipType').innerHTML = '泰納防具 ' + currentReforge + ' → ' + targetReforge;
+        document.getElementById('tReforgeMaterial_1').innerHTML = '泰納布洛斯氣息';
+        document.getElementById('tReforgeMaterial_2').innerHTML = '塔斯瑪氣息';
+        document.getElementById('tRequireDaysLbl_1').innerHTML = '-';
+        document.getElementById('tRequireDaysFullLbl_1').innerHTML = '所需天數 (2,100 / 週)';
+        document.getElementById('tRequireDaysLbl_2').innerHTML = '-';
+        document.getElementById('tRequireDaysFullLbl_2').innerHTML = '所需天數 (2,100 / 週)';
     }
 
     // Stop when input out of range.
     if ((currentReforge >= targetReforge) || (currentReforge > 21) || (targetReforge > 21)) {
-        const tipModal = new bootstrap.Modal(document.getElementById('tipModal'))
+        const tipModal = new bootstrap.Modal(document.getElementById('tipModal'));
         tipModal.show();
 
         return;
@@ -85,7 +85,7 @@ function executeCalc() {
             'magicCrystalCost': 0,
             'edCost': 0
         }
-    }
+    };
 
     // Calculate final result
     for (i = currentReforge ; i < targetReforge ; i++) {
@@ -103,30 +103,30 @@ function executeCalc() {
     }
 
     // Update display
-    document.getElementById("tExcTime").innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['reforgeTime']) + " 次";
-    document.getElementById("tExcQuestMat").innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['questMaterialCost'])
-    document.getElementById("tExcDropMat").innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['dropMaterialCost'])
-    document.getElementById("tExcMagicCrystal").innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['magicCrystalCost'])
-    document.getElementById("tExcED").innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['edCost'])
+    document.getElementById('tExcTime').innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['reforgeTime']) + ' 次';
+    document.getElementById('tExcQuestMat').innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['questMaterialCost']);
+    document.getElementById('tExcDropMat').innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['dropMaterialCost']);
+    document.getElementById('tExcMagicCrystal').innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['magicCrystalCost']);
+    document.getElementById('tExcED').innerHTML = numberToStringWithCommas(reforgeResult['exceptValue']['edCost']);
 
-    document.getElementById("tGuaTime").innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['reforgeTime']) + " 次"
-    document.getElementById("tGuaQuestMat").innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['questMaterialCost'])
-    document.getElementById("tGuaDropMat").innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['dropMaterialCost'])
-    document.getElementById("tGuaMagicCrystal").innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['magicCrystalCost'])
-    document.getElementById("tGuaED").innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['edCost'])
+    document.getElementById('tGuaTime').innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['reforgeTime']) + ' 次';
+    document.getElementById('tGuaQuestMat').innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['questMaterialCost']);
+    document.getElementById('tGuaDropMat').innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['dropMaterialCost']);
+    document.getElementById('tGuaMagicCrystal').innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['magicCrystalCost']);
+    document.getElementById('tGuaED').innerHTML = numberToStringWithCommas(reforgeResult['guaranteed']['edCost']);
 
     // Calculate spend time and update display
-    if (equipType === "Amethystine") {
+    if (equipType === 'Amethystine') {
         // Amethystine
-        document.getElementById("tRequireDays_1").innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 350, 2) + " 週"
-        document.getElementById("tRequireDaysFull_1").innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 550, 2) + " 週"
-        document.getElementById("tRequireDays_2").innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 350, 2) + " 週"
-        document.getElementById("tRequireDaysFull_2").innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 550, 2) + " 週"
+        document.getElementById('tRequireDays_1').innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 350, 2) + ' 週';
+        document.getElementById('tRequireDaysFull_1').innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 550, 2) + ' 週';
+        document.getElementById('tRequireDays_2').innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 350, 2) + ' 週';
+        document.getElementById('tRequireDaysFull_2').innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 550, 2) + ' 週';
     } else {
         // Tenebrous
-        document.getElementById("tRequireDays_1").innerHTML = "- 週"
-        document.getElementById("tRequireDaysFull_1").innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 2100, 2) + " 週"
-        document.getElementById("tRequireDays_2").innerHTML = "- 週"
-        document.getElementById("tRequireDaysFull_2").innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 2100, 2) + " 週"
+        document.getElementById('tRequireDays_1').innerHTML = '- 週';
+        document.getElementById('tRequireDaysFull_1').innerHTML = roundNumber(reforgeResult['exceptValue']['questMaterialCost'] / 2100, 2) + ' 週';
+        document.getElementById('tRequireDays_2').innerHTML = '- 週';
+        document.getElementById('tRequireDaysFull_2').innerHTML = roundNumber(reforgeResult['guaranteed']['questMaterialCost'] / 2100, 2) + ' 週';
     }
 }
